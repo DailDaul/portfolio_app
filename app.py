e8fc636ba325ba3b7c5ed125135934f47f73a164
Князева Таисия Игоревна <taisiyakn@gmail.com>
2026-06-04 14:38:03 +0300

    Update app.py
    добавление порта для деплоя на render

--- app.py
+++ app.py
@@ -751,4 +751,6 @@ with app.app_context():
         print('Создан администратор: admin / admin123')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
